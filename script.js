d0687c4ad8300a23fef63f55f8b93b31c771efdb
stevie2codes <stevie2codes@gmail.com>
2019-11-04 14:05:59 -0600

    Fixed function to output random chracters with proper length

--- script.js
+++ script.js
@@ -26,44 +26,38 @@ let passwordResult = '';
 function generate() {
   let userInput = prompt("Enter a number between 8 - 128 for length of password");
   let userSize = parseInt(userInput);
-  
-
-  if (userSize < 8 || userSize > 128 || isNaN(userSize)) {
-    alert("You must enter a valid number");
-  }
-  else {
   let specialOption = confirm("Would you like your password to contain special characters?");
   let numericOption = confirm("Would you like your password to contain numbers?");
   let lowerOption = confirm("would you like your password to contain lower case letters?");
   let upperOption = confirm("would you like your password to contain Upper case letters?");
 
+  if (userSize < 8 || userSize > 128 || isNaN(userSize)) {
+    alert("You must enter a valid number");
+  }
+  else {  
   if(specialOption) {
    charsConfirmed += specialSet;
-  }
+  }else
   if(numericOption) {
    charsConfirmed += numericSet ;
-  }
+  }else
   if(lowerOption) {
    charsConfirmed += lowerSet;
-  }
+  }else
   if(upperOption) {
    charsConfirmed += upperSet;
   }
-  }
-  
+} 
   passLength = charsConfirmed.length;
   
   function randomize(length){
      for(let i = 0; i <= length; i++) {
-    passwordResult += charsConfirmed.charAt(Math.floor(Math.random() * passLength));
+    passwordResult += charsConfirmed.charAt(Math.floor(Math.random() * userInput -1));
     }
     return passwordResult;
   }
-
+   document.getElementById("display").innerHTML = randomize(userInput);
   
-  document.getElementById("display").innerHTML = randomize(userInput);
-  
-
   }
 
   
